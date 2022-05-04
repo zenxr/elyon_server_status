657b2ead0f79fd5534f9036215e57ac60691fcef
Cody Stephenson <cdystphnsn@gmail.com>
2022-05-03 23:35:59 -0700

    Update to support multiple games and cleaner game configuration

--- config/configuration.py
+++ config/configuration.py
@@ -9,17 +9,3 @@ else:
     CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 
-server_info = {
-    "servers" : {
-        "North America" : {
-            "leticia" : "49.236.159.67",
-            "arquina" : "49.236.159.66"
-        },
-        "Europe" : {
-            "entara " : "37.48.82.178",
-            "lenoir" : "37.48.82.179"
-        }
-        
-    },
-    "port" : "7801"
-}
